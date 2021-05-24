69c8d6f7f7ee5887d1c78a7465bd826013aae946
marcoalonso <marcoalonsorota@hotmail.com>
2021-05-24 08:48:33 -0500

    Firebase Agregado

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -6,29 +6,19 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mensajeBienvendiaLebel: UILabel!
+    @IBOutlet weak var mensajeBienvendiaLebel: CLTypingLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mensajeBienvendiaLebel.text = ""
+        mensajeBienvendiaLebel.charInterval = 0.05
         
-        var indice = 0.0
-        
-        let tituloLabel = "Hola bienvenidos a la app oficial del Intituto Tecnologico de Morelia, puedes iniciar sesion o registrarte con tu correo institucional."
-        
-        for letra in tituloLabel {
-            print("-")
-            print(0.1 * indice)
-            print(letra)
-            
-            Timer.scheduledTimer(withTimeInterval: 0.1 * indice, repeats: false) { (timer) in
-                self.mensajeBienvendiaLebel.text?.append(letra)
-            }
-            indice += 1
-        }
+        mensajeBienvendiaLebel.text = "Hola bienvenidos a la app oficial del Intituto Tecnologico de Morelia, puedes iniciar sesion o registrarte con tu correo institucional."
+    
         
     }
 
