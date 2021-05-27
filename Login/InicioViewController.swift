ace205cd9917cb526bcaaf69d8bf33f4422d7fed
marcoalonso <marcoalonsorota@hotmail.com>
2021-05-27 08:58:40 -0500

    Lectura de Datos desde Firestore

--- Login/InicioViewController.swift
+++ Login/InicioViewController.swift
@@ -23,11 +23,55 @@ class InicioViewController: UIViewController {
         super.viewDidLoad()
         //ocultar el boton de regresar
         navigationItem.hidesBackButton = true
+        
+        cargarMensajes()
     }
+    
+    func cargarMensajes(){
+        db.collection("mensajes")
+            .order(by: "fechaCreacion")
+            .addSnapshotListener() { (querySnapshot, err) in
+                //Vaciar arreglo de chats
+                self.chats = []
+                
+            if let e = err {
+                print("Error al obtener los chats: \(e.localizedDescription)")
+            } else {
+                if let snapshotDocumentos = querySnapshot?.documents {
+                    for document in snapshotDocumentos {
+                        //crear mi objeto mensaje
+                        let datos = document.data()
+                        print(datos)
+                        // Sacar los parametros p obj Mensaje
+                        guard let remitenteFS = datos["remitente"] as? String else { return }
+                        guard let mensajeFS = datos["mensaje"] as? String else { return }
+                        
+                        let nuevoMensaje = Mensaje(remitente: remitenteFS, cuerpoMsj: mensajeFS)
+                        
+                        self.chats.append(nuevoMensaje)
+                        
+                        DispatchQueue.main.async {
+                            self.tablaChats.reloadData()
+                        }
+                       
+                     }
+                }
+                
+            }
+        }
+        
+        
+    }
+    
+    
     
     @IBAction func enviarButton(_ sender: UIButton) {
         if let mensaje = mensajeEnviarTF.text, let remitente = Auth.auth().currentUser?.email {
-            db.collection("mensajes").addDocument(data: ["remitente": remitente, "mensaje": mensaje]) { (error) in
+            db.collection("mensajes").addDocument(data: [
+                "remitente": remitente,
+                "mensaje": mensaje,
+                "fechaCreacion": Date().timeIntervalSince1970
+            ]) { (error) in
                 //si hubo errro
                 if let e = error {
                     print("Error al guardar en Firestore \(e.localizedDescription)")
